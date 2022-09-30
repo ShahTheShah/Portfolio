a26d34a115f9c50a834547b1159b0c5abc266f48
Shah <shahtheshah@ya.ru>
2022-09-30 20:25:49 +0300

    Update!

--- Layouts/Wildberries/source/assets/scripts/index.js
+++ Layouts/Wildberries/source/assets/scripts/index.js
@@ -68,8 +68,32 @@ const
     },
     fixedScrollProductAside = () => {
         const
-            aside = document.getElementById('product-aside'),
-            offsetRight = aside.offsetRight
+            fixedBlock = document.getElementById('product-aside-container'),
+            parentFixedBlock = document.getElementById('product-main-container'),
+            container = document.querySelector('.container'),
+            offsetRight = container.offsetLeft + container.clientWidth - fixedBlock.clientWidth,
+            parentFixedBlockTop = parentFixedBlock.getBoundingClientRect().top,
+            smallOffset = 20,
+            fixedScrollBlock = () => {
+                const
+                    scrollDistance = window.scrollY;
+                if (scrollDistance > (parentFixedBlockTop - smallOffset) && scrollDistance <= (parentFixedBlock.clientHeight + parentFixedBlockTop)) {
+                    fixedBlock.classList.remove('absolute');
+                    fixedBlock.style.width = `${fixedBlock.clientWidth}px`
+                    fixedBlock.style.left = `${offsetRight}px`;
+                    fixedBlock.classList.add('fixed');
+                } else {
+                    fixedBlock.style.left = `0px`;
+                    fixedBlock.classList.remove('fixed');
+                }
+                if (scrollDistance >= (parentFixedBlock.clientHeight + parentFixedBlockTop - fixedBlock.clientHeight - smallOffset)) {
+                    fixedBlock.style.left = `0px`;
+                    fixedBlock.classList.remove('fixed');
+                    fixedBlock.classList.add('absolute');
+                }
+            };
+        // fixedScrollBlock()
+        window.addEventListener('scroll', fixedScrollBlock);
     };
 sidebar();
 fixedScrollProductAside();
