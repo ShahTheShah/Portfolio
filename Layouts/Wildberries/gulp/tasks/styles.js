e939a83ff93ae5ccb2d96b3c5ac26347e1107747
Shah <shahtheshah@ya.ru>
2022-10-01 19:26:08 +0300

    hnn

--- Layouts/Wildberries/gulp/tasks/styles.js
+++ Layouts/Wildberries/gulp/tasks/styles.js
@@ -11,10 +11,10 @@ export default () => app.gulp.src(app.path.source.styles, { sourcemaps: app.isDe
         outputStyle: 'expanded'
     }))
     .pipe(app.plugins.if(app.isBuild, app.plugins.groupCssMedisQueries()))
-    .pipe(app.plugins.autoprefixer({
-        grid: true,
-        overrideBrowserslist: ['last 3 versions']
-    }))
+    // .pipe(app.plugins.autoprefixer({
+    //     // grid: true,
+    //     // overrideBrowserslist: ['last 2 versions']
+    // }))
     .pipe(app.plugins.replace(/@images\//g, '../images/'))
     .pipe(app.gulp.dest(app.path.build.styles))
     .pipe(app.plugins.if(app.isBuild, app.plugins.cleanCss()))
